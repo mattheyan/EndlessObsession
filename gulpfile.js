ac28db639f784b95b34583403c5680d605e0f4f6
Bryan Matthews <mattheyan@gmail.com>
2015-04-17 19:13:19 -0400

    add gulp tasks for build and preview

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var wintersmith = require('wintersmith');
+
+gulp.task('build:release', function(callback) {
+	var env = wintersmith('./config-release.json');
+	env.build(function(error) {
+		if (error) {
+			throw new Error(error);
+		}
+		callback();
+	});
+});
+
+gulp.task('preview', function() {
+	var env = wintersmith('./config-dev.json');
+	env.preview(function(error) {
+		if (error) {
+			throw new Error(error);
+		}
+	});
+});
