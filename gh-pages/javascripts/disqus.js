58118d8a5bc1bda6e7e14e31fa2b7f29f5274993
Bryan Matthews <mattheyan@gmail.com>
2022-03-12 16:13:14 -0500

    Add generated site from 'gh-pages' branch

---
+++ gh-pages/javascripts/disqus.js
@@ -0,0 +1,13 @@
+(function () {
+	var scriptTags = document.getElementsByTagName("script");
+	var referrer = scriptTags[scriptTags.length - 1].src;
+
+	var match = /[?&]name=([^&]*)/i.exec(referrer);
+	if (match) {
+		var disqusName = match[1];
+
+		var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+		dsq.src = 'http://' + disqusName + '.disqus.com/embed.js';
+		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+	}
+}());
