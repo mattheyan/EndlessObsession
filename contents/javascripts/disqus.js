fa88861994037fa3364104310cc37d179afc0d92
Bryan Matthews <mattheyan@gmail.com>
2022-03-12 19:50:17 -0500

    Change disqus script to https

--- contents/javascripts/disqus.js
+++ contents/javascripts/disqus.js
@@ -7,7 +7,7 @@
 		var disqusName = match[1];
 
 		var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-		dsq.src = 'http://' + disqusName + '.disqus.com/embed.js';
+		dsq.src = 'https://' + disqusName + '.disqus.com/embed.js';
 		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 	}
 }());
