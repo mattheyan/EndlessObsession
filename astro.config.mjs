d9806f90634fcb2a88410fed6207569e4212ae29
Bryan Matthews <bryan@cognitoforms.com>
2024-02-17 11:58:14 -0500

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'http://localhost:8888/',
     server: {
         port: 8888
+    },
+    build: {
+        inlineStylesheets: 'never'
+    },
+    markdown: {
+        syntaxHighlight: 'prism'
     }
 });
